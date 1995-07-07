73854c9a239ac7370861781eb42fd499c9db2465
Gisle Aas <aas@oslonett.no>
1995-07-07 15:16:16 +0000

    Renamed to File::Counter. Remebers its last value.

--- lib/File/CounterFile.pm
+++ lib/File/CounterFile.pm
@@ -1,18 +1,18 @@
 # This -*-perl -*- module implements a persistent counter class.
 #
-# $Id: CounterFile.pm,v 0.2 1995/07/07 11:29:37 aas Exp $
+# $Id: CounterFile.pm,v 0.3 1995/07/07 15:16:16 aas Exp $
 #
 
-package Counter;
+package File::Counter;
 
 =head1 NAME
 
-Counter - Persistent counter class
+File::Counter - Persistent counter class
 
 =head1 SYNOPSIS
 
- use Counter;
- $c = new Counter "COUNTER", "aa00";
+ use File::Counter;
+ $c = new File::Counter "COUNTER", "aa00";
 
  $id = $c->inc;
  open(F, ">F$id");
@@ -25,10 +25,10 @@ name as a parameter to the object constructor.  The file is created if
 it does not exist.
 
 If the file name does not start with "/" or ".", then it is
-interpreted as a file relative to C<$Counter::DEFAULT_DIR>.  You might
-pass a second parameter to the constructor, that sets the initial
-value for a new counter.  This parameter only takes effect when the
-file is created (i.e. it does not exist before the call).
+interpreted as a file relative to C<$File::Counter::DEFAULT_DIR>.  You
+might pass a second parameter to the constructor, that sets the
+initial value for a new counter.  This parameter only takes effect
+when the file is created (i.e. it does not exist before the call).
 
 Each time you call the C<inc> method, you increment the counter value.
 The new value is returned.
@@ -36,7 +36,7 @@ The new value is returned.
 =head1 BUGS
 
 It uses flock(2) to lock the counter file.  This does not always
-work.
+work.  Perhaps it should use the File::Lock module.
 
 =head1 AUTHOR
 
@@ -47,11 +47,18 @@ Gisle Aas <aas@oslonett.no>
 
 use Carp;
 
-$VERSION = '1.0';
+$VERSION = $VERSION  # shut up -w
+    = (qw$Revision: 0.3 $)[1];
 
-$MAGIC           = "#COUNTER-$VERSION\n";
-$DEFAULT_DIR     = "/usr/tmp";
-$DEFAULT_INITIAL = 0;
+$MAGIC           = "#COUNTER-1.0\n";   # first line in counter files
+$DEFAULT_DIR     = "/usr/tmp";         # default location for counter files
+$DEFAULT_INITIAL = 0;                  # default initial counter value
+
+# Experimental overloading.  It does not work as good as expected.
+#
+# %OVERLOAD = ('++' => sub { my $this=shift; $this->inc; $this; },
+#   	       '""' => sub { shift->value; },
+# );
 
 
 sub new
@@ -62,21 +69,25 @@ sub new
     $file = "$DEFAULT_DIR/$file" unless $file =~ /^[\.\/]/;
     $initial = $DEFAULT_INITIAL unless defined $initial;
 
+    my $value;
     if (-e $file) {
 	croak("Specified file is a directory") if -d _;
 	open(F, $file) or croak("Can't open $file: $!");
 	my $first_line = <F>;
+	$value = <F>;
 	close(F);
-	croak("Bad magic in $file")
+	croak("Bad counter magic in $file")
 	    unless $first_line eq $MAGIC;
+	chomp($value);
     } else {
 	open(F, ">$file") or croak("Can't open $file: $!");
 	print F $MAGIC;
 	print F $initial, "\n";
 	close(F);
+	$value = $initial;
     }
 
-    bless { file => $file };
+    bless { file => $file, value => $value };
 }
 
 
@@ -84,23 +95,30 @@ sub inc
 {
     # Get a new identifier by incrementing the $count file
     my($this) = @_;
-    croak("Not a ref") unless ref($this) eq "Counter";
+    croak("Not a ref") unless ref($this) eq "File::Counter";
     my $file = $this->{file};
-    my $id = 0;
+    my $value;
     open(COUNT, "+<$file") or croak("Can't open $file: $!");
     flock(COUNT, 2) or croak("Can't flock: $!"); # exlusive lock
     my $magic = <COUNT>;
     if ($magic ne $MAGIC) {
 	close(COUNT);
-	croak("Bad magic '$magic' in $file");
+	croak("Bad counter magic '$magic' in $file");
     }
-    chomp($id = <COUNT>);
+    chomp($value = <COUNT>);
     seek(COUNT, 0, 0) or croak("Can't seek to beginning: $!");
-    $id++;
+    $value++;
     print COUNT $MAGIC;
-    print COUNT "$id\n";
+    print COUNT "$value\n";
     close(COUNT);
-    $id;
+    $this->{value} = $value;
+    $value;
+}
+
+
+sub value
+{
+    shift->{value};
 }
 
 1;

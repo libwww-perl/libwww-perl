1524a69b39dfa3621f2288558e532698a4a6dba3
Gisle Aas <aas@oslonett.no>
1995-08-21 18:46:25 +0000

    First revision.

---
+++ lib/LWP/Mailcap.pm
@@ -0,0 +1,311 @@
+#
+# $Id: Mailcap.pm,v 1.1 1995/08/21 18:46:25 aas Exp $
+
+package LWP::Mailcap;
+
+=head1 NAME
+
+LWP::Mailcap - Parse mailcap files
+
+=head1 DESCRIPTION
+
+Parse mailcap files as specified in RFC 1524 - I<A User Agent
+Configuration Mechanism For Multimedia Mail Format Information>.  In
+the description below C<$type> refers to the MIME type as specified in
+the I<Content-Type> header of mail or HTTP messages.  Examples of
+types are:
+
+  image/gif
+  text/html
+  text/plain; charset=iso-8859-1
+
+=cut
+
+$DEBUG = 1;
+$useCache = 1;
+
+my @path = split(/:/, $ENV{MAILCAPS} ||
+  "$ENV{HOME}/.mailcap:/etc/mailcap:/usr/etc/mailcap:/usr/local/etc/mailcap");
+  # this path is specified under RFC 1524 appendix A 
+
+
+=head1 METHODS
+
+=head2 new()
+
+  $mcap = new LWP::Mailcap;
+  $mcap = new LWP::Mailcap "/mydir/mailcap";
+
+Create and initialize a new LWP::Mailcap object.  If you give it an
+argument it will try to parse the specified file.  Without any
+arguments it will search for the mailcap file using the standard
+mailcap path, or the MAILCAPS environment variable.
+
+=cut
+
+sub new
+{
+    my($class, $file) = @_;
+    unless (defined $file) {
+	for (@path) {
+	    if (-r $_) {
+		$file = $_;
+		last;
+	    }
+	}
+    }
+    my $self = bless {}, $class;
+    if (defined $file && open(MAILCAP, $file)) {
+	$self->{'_file'} = $file;
+	while (<MAILCAP>) {
+	    next if /^\s*#/; # comment
+	    next if /^\s*$/; # blank line
+	    while (s/\\\s*$//) {  # continuation line
+		$_ .= <MAILCAP>;
+	    }
+	    chomp;
+	    s/\t/ /g;  # ensure no tabs in line
+	    s/([^\\]);/$1\t/g;
+	    my @parts = split(/ *\t */, $_);
+	    my $type = shift(@parts);
+	    $type .= "/*" unless $type =~ m,/,;
+	    my $view = shift(@parts);
+	    $view =~ s/\\;/;/g;
+	    my %field = ('view' => $view);
+	    for (@parts) {
+		my($key,$val) = split(/\s*=\s*/, $_, 2);
+		if (defined $val) {
+		    $val =~ s/\\;/;/g;
+		} else {
+		    $val = 1;
+		}
+		$field{$key} = $val;
+	    }
+	    if ($field{'test'}) {
+		my $test = $field{'test'};
+		unless ($test =~ /%/) {
+		    # No parameters in test, can perform it right away
+		    print "Running: $test\n" if $DEBUG;
+		    system $test;
+		    next if $?;
+		}
+	    }
+	    # record this entry
+	    unless (exists $self->{$type}) {
+		$self->{$type} = [];
+	    }
+	    push(@{$self->{$type}}, \%field);
+	}
+	close(MAILCAP);
+    } else {
+	# Set up default mailcap
+	$self->{'audio/*'}{'view'} = "showaudio %s";
+	$self->{'image/*'}{'view'} = "xv %s";
+	$self->{'message/rfc822'}{'view'} = "xterm -e metamail %s";
+    }
+    $self;
+}
+
+=head2 view($type, $file)
+
+=head2 compose($type, $file)
+
+=head2 edit($type, $file)
+
+=head2 print($type, $file)
+
+These methods invoke a suitable progam presenting or manipulating the
+media object in the specified file.  They all return C<1> if a command
+was found, and C<0> otherwise.
+
+=cut
+
+sub view       { my $self = shift; $self->_run($self->viewCmd(@_));    }
+sub compose    { my $self = shift; $self->_run($self->composeCmd(@_)); }
+sub edit       { my $self = shift; $self->_run($self->editCmd(@_));    }
+sub print      { my $self = shift; $self->_run($self->printCmd(@_));   }
+
+=head2 viewCmd($type, $file)
+
+=head2 composeCmd($type, $file)
+
+=head2 editCmd($type, $file)
+
+=head2 printCmd($type, $file)
+
+These methods return a string that is suitable for feeding to system()
+in order to invoke a suitable progam presenting or manipulating the
+media object in the specified file.  It will return C<undef> if no
+suitable specification exists.
+
+=cut
+
+sub viewCmd    { shift->_createCommand('view', @_);    }
+sub composeCmd { shift->_createCommand('compose', @_); }
+sub editCmd    { shift->_createCommand('edit', @_);    }
+sub printCmd   { shift->_createCommand('print', @_);   }
+
+sub _createCommand
+{
+    my($self, $method, $type, $file) = @_;
+    my $entry = $self->getEntry($type, $file);
+    return undef unless $entry;
+    if (exists $entry->{$method}) {
+	$cmd = $entry->{$method};
+	$cmd =~ s/\\%/\t/g;  # hide all escaped %'s
+	$cmd =~ s/%t/$type/g;
+	$cmd =~ s/%s/$file/g;
+	{
+	    local($^W) = 0;  # avoid warnings when expanding %params
+	    $cmd =~ s/%\{\s*(.*?)\s*\}/$params{$1}/g;
+	}
+	$cmd =~ s/\t/%/g;
+	return $cmd;    
+    } else {
+	return undef;
+    }
+}
+
+sub _run
+{
+    my($self, $cmd) = @_;
+    print "Runing: $cmd\n" if $DEBUG;
+    if (defined $cmd) {
+	system $cmd;
+	return 1;
+    }
+    0;
+}
+
+sub makeName
+{
+    my($self, $type, $basename) = @_;
+    my $template = $self->nametemplate($type);
+    return $basename unless $template;
+    $template =~ s/%s/$basename/g;
+    $template;
+}
+
+=head2 field($type, $field)
+
+Returns the specified field for the type.  Returns undef if no
+specification exsists.
+
+=cut
+
+sub field
+{
+    my($self, $type, $field) = @_;
+    my $entry = $self->getEntry($type);
+    $entry->{$field};
+}
+
+=head2 description($type)
+
+=head2 textualnewlines($type)
+
+=head2 x11_bitmap($type)
+
+=head2 nametemplate($type)
+
+These methods return the corresponding mailcap field for the type.
+These methods are more convenient to use than the field() method for
+the same fields.
+
+=cut
+
+sub description     { shift->field(shift, 'description');     }
+sub textualnewlines { shift->field(shift, 'textualnewlines'); }
+sub x11_bitmap      { shift->field(shift, 'x11-bitmap');      }
+sub nametemplate    { shift->field(shift, 'nametemplate');    }
+
+sub getEntry
+{
+    my($self, $origtype, $file) = @_;
+
+    if ($useCache) {
+	if (exists $self->{'_cache'}{$origtype}) {
+	    print "Using cache for $origtype\n";
+	    return $self->{'_cache'}{$origtype};
+	}
+    }
+
+    $file = "" unless defined $file;
+    my($fulltype, @params) = split(/\s*;\s*/, $origtype);
+    my($type, $subtype) = split(/\//, $fulltype, 2);
+    my %params;
+    for (@params) {
+	my($key,$val) = split(/\s*=\s*/, $_, 2);
+	$params{$key} = $val;
+    }
+
+    my $entry;
+    for (@{$self->{"$type/$subtype"}}, @{$self->{"$type/*"}}) {
+	if (exists $_->{'test'}) {
+	    # must run test to see if it applies
+	    my $test = $_->{'test'};
+	    $test =~ s/\\%/\t/g;  # hide all escaped %'s
+	    $test =~ s/%t/$fulltype/g;
+	    $test =~ s/%s/$file/g;
+	    {
+		local($^W) = 0;  # avoid warnings when expanding %params
+		$test =~ s/%\{\s*(.*?)\s*\}/$params{$1}/g;
+	    }
+	    $test =~ s/\t/%/g;
+	    print "Running: $test\n" if $DEBUG;
+	    system $test;
+	    next if $?;
+	}
+	$entry = { %$_ };  # make copy
+        last;
+    }
+    $self->{'_cache'}{$origtype} = $entry if $useCache;
+    $entry;
+}
+
+
+# This following procedures can be useful for debugging purposes
+
+sub dumpEntry
+{
+    my($hash, $prefix) = @_;
+    $prefix = "" unless defined $prefix;
+    for (sort keys %$hash) {
+	print "$prefix$_ = $hash->{$_}\n";
+    }
+}
+
+sub dump
+{
+    my($self) = @_;
+    for (keys %$self) {
+	next if /^_/;
+	print "$_\n";
+	for (@{$self->{$_}}) {
+	    dumpEntry($_, "\t");
+	    print "\n";
+	}
+    }
+    if (exists $self->{'_cache'}) {
+	print "Cached types\n";
+	for (keys %{$self->{'_cache'}}) {
+	    print "\t$_\n";
+	}
+    }
+}
+
+=head1 COPYRIGHT
+
+Copyright (c) 1995 Gisle Aas. All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Gisle Aas <aas@oslonett.no>
+
+=cut
+
+
+1;

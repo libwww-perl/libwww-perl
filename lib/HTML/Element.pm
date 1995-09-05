84f0717639af0bc82ba9f1d2344d45ff89f41e24
Gisle Aas <aas@oslonett.no>
1995-09-05 22:10:25 +0000

    Dump to STDOUT.  Added documentation for all methods.

--- lib/HTML/Element.pm
+++ lib/HTML/Element.pm
@@ -1,10 +1,10 @@
 package HTML::Element;
 
-# $Id: Element.pm,v 1.3 1995/09/05 14:33:35 aas Exp $
+# $Id: Element.pm,v 1.4 1995/09/05 22:10:25 aas Exp $
 
 =head1 NAME
 
-HTML::Element - Ditto
+HTML::Element - Class for objects that represent HTML elements
 
 =head1 SYNOPSIS
 
@@ -25,27 +25,26 @@ HTML::Element - Ditto
 
 Objects of the HTML::Element class can be used to represent elements
 of HTML.  Objects have attributes and content.  The content is a
-sequence of text segments and other HTML::Element objects.
+sequence of text segments and other HTML::Element objects.  Thus a
+tree of HTML::Element objects as nodes can represent the syntax tree
+for a HTML document.
 
-=head1 COPYRIGHT
+The following methods are available:
 
-Copyright (c) 1995 Gisle Aas. All rights reserved.
-
-This library is free software; you can redistribute it and/or
-modify it under the same terms as Perl itself.
-
-=head1 AUTHOR
-
-Gisle Aas <aas@oslonett.no>
+=over 4
 
 =cut
 
 
 use Carp;
 
+$VERSION = sprintf("%d.%02d", q$Revision: 1.4 $ =~ /(\d+)\.(\d+)/);
+sub Version { $VERSION; }
+
+
 # Elements that does not have corresponding end tags
 for (qw(base link meta isindex nextid
-	img br nobr wbr hr
+	img br hr wbr
 	input
        )
     ) {
@@ -62,6 +61,14 @@ for (qw(base link meta isindex nextid
  'link' => 'href',
 );
 
+
+
+=item new HTML::Element 'tag', 'attrname' => 'value',...
+
+The object constructor.  Takes an tag name as argument. Optionally
+allows you to specify initial attributes at object creation time.
+
+=cut
 
 sub new
 {
@@ -81,10 +88,26 @@ sub new
     $self;
 }
 
+
+
+=item ->tag()
+
+Returns the tag name for the element.
+
+=cut
+
 sub tag
 {
     shift->{_tag};
 }
+
+
+
+=item ->starttag()
+
+Returns the complete start tag for the element.  Including <> and attributes.
+
+=cut
 
 sub starttag
 {
@@ -104,21 +127,55 @@ sub starttag
     "$tag>";
 }
 
+
+
+=item ->endtag()
+
+Returns the complete end tag.
+
+=cut
+
 sub endtag
 {
     "</\U$_[0]->{_tag}>";
 }
 
 
+
+=item ->parent([$newparent])
+
+Returns (optionally sets) the parent for this element.
+
+=cut
+
 sub parent
 {
     shift->attr('_parent', @_);
 }
 
+
+
+=item ->implicit([$bool])
+
+Returns (optionally sets) the implicit attribute.  This attribute is
+used to indicate that the element was not originally present in the
+source, but was inserted in order to conform to HTML strucure.
+
+=cut
+
 sub implicit
 {
     shift->attr('_implicit', @_);
 }
+
+
+
+=item ->isInside('tag')
+
+Returns true if this tag is contained inside the specified tag.
+
+=cut
+
 sub isInside
 {
     my($self, $tag) = @_;
@@ -130,12 +187,28 @@ sub isInside
     0;
 }
 
+
+
+=item ->pos()
+
+Returns (and optionally sets) the current position.
+
+=cut
+
 sub pos
 {
     my $self = shift;
     my $pos = $self->attr('_pos', @_);
     $pos || $self;
 }
+
+
+
+=item ->attr('attr', [$value])
+
+Returns (and optionally sets) the value of some attribute.
+
+=cut
 
 sub attr
 {
@@ -148,16 +221,42 @@ sub attr
     $old;
 }
 
+
+
+=item ->content()
+
+Returns the content of this element.  The content is represented as a
+array of text segments and references to other HTML::Element objects.
+
+=cut
+
 sub content
 {
     shift->{'_content'};
 }
+
+
+
+=item ->isEmpty()
+
+Returns true if there is no content.
+
+=cut
 
 sub isEmpty
 {
     my $self = shift;
     !exists($self->{'_content'}) || !@{$self->{'_content'}};
 }
+
+
+
+=item ->pushContent($element)
+
+Adds to the content of the element.  The content should be a text
+segment (scalar) or a reference to a HTML::Element object.
+
+=cut
 
 sub pushContent
 {
@@ -167,7 +266,15 @@ sub pushContent
     $self;
 }
 
-sub deleteContent
+
+
+=item ->deleteContent
+
+Clears the content.
+
+=cut
+
+sub deleteContent()
 {
     my $self = shift;
     for (@{$self->{'_content'}}) {
@@ -177,6 +284,16 @@ sub deleteContent
     $self;
 }
 
+
+
+=item ->delete()
+
+Frees memory assosiated with the element an all children.  This is
+needed because perl's reference counting does not work since we use
+circular references.
+
+=cut
+
 sub delete
 {
     $_[0]->deleteContent;
@@ -184,6 +301,17 @@ sub delete
     delete $_[0]->{_pos};
     $_[0] = undef;
 }
+
+
+
+=item ->traverse(\&callback, [$ignoretext])
+
+Traverse the element and all its children.  For each node visited, the
+callback routine is called with the node and the depth as arguments.
+If the $ignoretext parameter is true, then the callback will not be
+called for text content.
+
+=cut
 
 sub traverse
 {
@@ -200,6 +328,26 @@ sub traverse
     }
     $self;
 }
+
+
+
+=item ->extractLinks([@wantedTypes])
+
+Returns links found by traversing the element and all its children.
+The return value is a reference to an array.  Each element of the
+array is an array with 2 values; the link value and a reference to the
+corresponding element.
+
+You might specify that you just want to extract some types of links.
+For instance if you only want to extract <a href="..."> and <img
+src="..."> links you might code it like this:
+
+  for (@{ $e->extractLinks(qw(a img)) }) {
+      ($link, $linkelem) = @$_;
+      ...
+  }
+
+=cut
 
 sub extractLinks
 {
@@ -224,21 +372,39 @@ sub extractLinks
     \@links;
 }
 
+
+
+=item ->dump()
+
+Prints the element and all its children to STDOUT.  Mainly useful for
+debugging.
+
+=cut
+
 sub dump
 {
     my $self = shift;
     my $depth = shift || 0;
-    print "  " x $depth;
-    print $self->starttag, "\n";
+    print STDERR "  " x $depth;
+    print STDERR $self->starttag, "\n";
     for (@{$self->{_content}}) {
 	if (ref $_) {
 	    $_->dump($depth+1);
 	} else {
-	    print "  " x ($depth + 1);
-	    print qq{"$_"\n};
+	    print STDERR "  " x ($depth + 1);
+	    print STDERR qq{"$_"\n};
 	}
     }
 }
+
+
+
+=item ->asHTML()
+
+Retuns a string (the HTML document) that represents the element and
+its children.
+
+=cut
 
 sub asHTML
 {
@@ -286,3 +452,20 @@ sub asHTML
 
 
 1;
+
+__END__
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright (c) 1995 Gisle Aas. All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Gisle Aas <aas@oslonett.no>
+
+=cut

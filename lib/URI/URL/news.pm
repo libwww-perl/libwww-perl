6208da7b7402f1a8307584d2c5f06602edf4f40f
Gisle Aas <aas@oslonett.no>
1996-02-26 18:47:39 +0000

    New implementation for specific access methods.

--- lib/URI/URL/news.pm
+++ lib/URI/URL/news.pm
@@ -8,20 +8,49 @@ sub new {
     my($class, $init, $base) = @_;
     my $self = bless { }, $class;
     $self->{'scheme'}  = lc($1) if $init =~ s/^\s*([\w\+\.\-]+)://;
-    my $tmp = uri_unescape($init);
-    $self->{'grouppart'} = $tmp;
-    $self->{ ($tmp =~ m/\@/) ? 'article' : 'group' } = $tmp;
+    $self->groupart(uri_unescape($init));
     $self->base($base) if $base;
     $self;
 }
 
-sub grouppart { shift->_elem('grouppart', @_) }
-sub article   { shift->_elem('article',   @_) }
-sub group     { shift->_elem('group',     @_) }
+sub groupart {
+    my $self = shift;
+    my $old = $self->{'path'};
+    if (@_) {
+	my $p = shift;
+	if (defined $p && $p =~ /\@/) {
+	    # it is an message id
+	    $p =~ s/^<(.*)>$/$1/;  # "<" and ">" should not be part of it
+	}
+	$self->{'path'} = $p;
+    }
+    $old;
+}
+
+*path = \&groupart;
+
+sub article   {
+    my $self = shift;
+    Carp::croak("Illegal article id name (does not contain '\@')")
+      if @_ && $_[0] !~ /\@/;
+    my $old = $self->groupart(@_);
+    return undef if $old !~ /\@/;
+    $old;
+}
+
+
+sub group {
+    my $self = shift;
+    Carp::croak("Illegal group name (contains '\@')")
+      if @_ && $_[0] =~ /\@/;
+    my $old = $self->groupart(@_);
+    return undef if $old =~ /\@/;
+    $old;
+}
 
 sub as_string {
     my $self = shift;
-    "$self->{'scheme'}:" . uri_escape($self->{'grouppart'});
+    "$self->{'scheme'}:" . uri_escape($self->{'path'});
 }
 
 1;

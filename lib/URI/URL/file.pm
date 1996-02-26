35764a6167ef547f27b6605beb1bfcebf09da908
Gisle Aas <aas@oslonett.no>
1996-02-26 18:50:12 +0000

    Better Unix file handling.  Path might contain a leading slash.

--- lib/URI/URL/file.pm
+++ lib/URI/URL/file.pm
@@ -13,9 +13,8 @@ OS: {
     $ostype = 'mac', last if $os eq "Mac";
     $ostype = 'unix';  # The default
 }
-# If you add more types to this list, remember to add a xxx_path method
-# as well.
-
+# NOTE: If you add more types to this list, remember to add a xxx_path
+# method as well.
 
 # This is the BNF found in RFC 1738:
 #
@@ -24,10 +23,40 @@ OS: {
 # fsegment       = *[ uchar | "?" | ":" | "@" | "&" | "=" ]
 # Note that fsegment can contain '?' (query) but not ';' (param)
 
+sub _parse {
+    my($self, $init) = @_;
+    # The file URL can't have query
+    $self->URI::URL::_generic::_parse($init, qw(netloc path params frag));
+}
+
+# sub local_path { ... }
+#
+# Returns a path suitable for use on the local system (we just
+# set up an alias (derived from $ostype) to one of the path methods
+# defined below)
+*local_path = \&{$ostype . "_path"};
+
+sub query { Carp::croak("Illegal method for file URLs"); }
+*equery = \&query;
+
+# A U T O  L O A D E R
+# Don't remove this comment, it keeps AutoSplit happy!!
+# @ISA = qw(AutoLoader)
+#
+# These methods are autoloaded as needed
+sub newlocal;
+sub unix_path;
+sub dos_path ;
+sub mac_path ;
+sub vms_path ;
+1;
+__END__
+
 sub newlocal {
     my($class, $path) = @_;
 
-    Carp::Croak("Only implemented for Unix") unless $ostype eq "unix";
+    Carp::Croak("Only implemented for Unix file systems")
+      unless $ostype eq "unix";
     # XXX: Should implement the same thing for other systems
 
     my $url = new URI::URL "file:";
@@ -41,33 +70,25 @@ sub newlocal {
     $url;
 }
 
-sub _parse {
-    my($self, $init) = @_;
-    # The file URL can't have query
-    $self->URI::URL::_generic::_parse($init, qw(netloc path params frag));
-    1;
-}
-
-# Returns a path suitable for use on the local system
-eval <<"EOT";
-sub local_path
-{
-    shift->${ostype}_path;
-}
-EOT
-die $@ if $@;
-
-
 sub unix_path
 {
     my $self = shift;
     my @p;
     for ($self->path_components) {
-	Carp::croak("Path component contains '/'") if m|/|;
+	Carp::croak("Path component contains '/' or '\0'") if m|[\0/]|;
+	if (@p) {
+	    next unless length $_;   # skip empty path segments
+	    next if $_ eq '.';       # skip these too
+	    if ($_ eq '..' && $p[-1] ne '..') {  # go up one level
+		pop(@p) if $p[-1] ne '';
+		next;
+	    }
+	}
 	push(@p, $_);
     }
-    unshift(@p, '') if $self->absolute_path;
-    my $p = join('/', @p);
+    shift(@p) if @p > 1 && $p[0] eq '.';   # './' rendundant if there is more
+    return '/' if !@p || (@p == 1 && $p[0] eq '');
+    join('/', @p);
 }
 
 sub dos_path
@@ -78,7 +99,6 @@ sub dos_path
 	Carp::croak("Path component contains '/' or '\\'") if m|[/\\]|;
 	push(@p, uc $_);
     }
-    unshift(@p, '') if $self->absolute_path;
     my $p = join("\\", @p);
     $p =~ s/^\\([A-Z]:)/$1/;  # Fix drive letter specification
     $p;
@@ -90,14 +110,25 @@ sub mac_path
     my @p;
     for ($self->path_components) {
 	Carp::croak("Path component contains ':'") if /:/;
+	# XXX: Should probably want to do something about ".." and "."
+	# path segments.  I don't know how these are represented in
+	# the Machintosh file system.  If these are valid file names
+	# then we should split the path ourself, as $u->path_components
+	# loose the distinction between '.' and '%2E'.
 	push(@p, $_);
     }
-    unshift(@p, '') unless $self->absolute_path;  # Macs to it this way
+    if (@p && $p[0] eq '') {
+	shift @p;
+    } else {
+	unshift(@p, '');
+    }
     join(':', @p);
 }
 
 sub vms_path
 {
+    # ????? Can some VMS people please redo this function ??????
+
     # This is implemented based on what RFC1738 (sec 3.10) says in the
     # VMS file example:
     #
@@ -107,10 +138,15 @@ sub vms_path
     #
     #  file:/disk$user/my/notes/note12345.txt
     #
-    # BEWARE: I don't have a VMS machine myself so this is pure guesswork
+    # BEWARE: I don't have a VMS machine myself so this is pure guesswork!!!
 
     my $self = shift;
     my @p = $self->path_components;
+    my $abs = 0;
+    if (@p && $p[0] eq '') {
+	shift @p;
+	$abs = 1;
+    }
     # First I assume there must be a dollar in a disk spesification
     my $p = '';
     $p = uc(shift(@p)) . ":"  if @p && $p[0] =~ /\$/;
@@ -118,12 +154,9 @@ sub vms_path
     $p .= "[" . join(".", map{uc($_)} @p) . "]" if @p;
     $p .= uc $file;
     # XXX: How is an absolute path different from a relative one??
-    $p =~ s/\[/[./ unless $self->absolute_path;  # Like this???
+    $p =~ s/\[/[./ unless $abs;
     # XXX: How is a directory denoted??
     $p;
 }
-
-sub query { Carp::croak("Illegal method for file URLs"); }
-*equery = \&query;
 
 1;

3f15e87ab3837a3646432032a2513ac7a4b60448
Gisle Aas <aas@oslonett.no>
1996-07-17 08:50:52 +0000

    Updated according to draft-ietf-http-v11-spec-05

--- lib/HTTP/Status.pm
+++ lib/HTTP/Status.pm
@@ -1,5 +1,5 @@
 #
-# $Id: Status.pm,v 1.16 1996/04/09 15:44:23 aas Exp $
+# $Id: Status.pm,v 1.17 1996/07/17 08:50:52 aas Exp $
 
 package HTTP::Status;
 
@@ -25,7 +25,8 @@ HTTP::Status - HTTP Status code processing
 
 HTTP::Status is a library of routines for defining and classification
 of HTTP status codes for libwww-perl.  Status codes are used to encode
-the overall outcome of a HTTP response message.
+the overall outcome of a HTTP response message.  Codes correspond to
+those defined in E<lt>draft-ietf-http-v11-spec-05>.
 
 The following functions can be used as mnemonic status code names:
 
@@ -50,18 +51,22 @@ The following functions can be used as mnemonic status code names:
    RC_FORBIDDEN
    RC_NOT_FOUND
    RC_METHOD_NOT_ALLOWED
-   RC_NONE_ACCEPTABLE
+   RC_NOT_ACCEPTABLE
    RC_PROXY_AUTHENTICATION_REQUIRED
    RC_REQUEST_TIMEOUT
    RC_CONFLICT
    RC_GONE
    RC_LENGTH_REQUIRED
-   RC_UNLESS_TRUE
+   RC_PRECONDITION_FAILED
+   RC_REQUEST_ENTITY_TOO_LARGE
+   RC_REQUEST_URI_TOO_LARGE
+   RC_UNSUPPORTED_MEDIA_TYPE
    RC_INTERNAL_SERVER_ERROR
    RC_NOT_IMPLEMENTED
    RC_BAD_GATEWAY
    RC_SERVICE_UNAVAILABLE
    RC_GATEWAY_TIMEOUT
+   RC_HTTP_VERSION_NOT_SUPPORTED
 
 The status_message() function will translate status codes to human
 readable strings.
@@ -103,18 +108,22 @@ my %StatusCode = (
     403 => 'Forbidden',
     404 => 'Not Found',
     405 => 'Method Not Allowed',
-    406 => 'None Acceptable',
+    406 => 'Not Acceptable',
     407 => 'Proxy Authentication Required',
     408 => 'Request Timeout',
     409 => 'Conflict',
     410 => 'Gone',
     411 => 'Length Required',
-    412 => 'Unless True',
+    412 => 'Precondition Failed',
+    413 => 'Request Entity Too Large',
+    414 => 'Request-URI Too Large',
+    415 => 'Unsupported Media Type',
     500 => 'Internal Server Error',
     501 => 'Not Implemented',
     502 => 'Bad Gateway',
     503 => 'Service Unavailable',
     504 => 'Gateway Timeout',
+    505 => 'HTTP Version not supported',
 );
 
 my $mnemonicCode = '';

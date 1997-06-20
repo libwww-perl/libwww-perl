fb36b0024a1ade5e3951c369b62bf88ccffcc102
Gisle Aas <aas@oslonett.no>
1997-06-20 09:52:23 +0000

    Escape more when setting $u->query_form.  Treat reserved "+" as space when decoding.

--- lib/URI/URL/http.pm
+++ lib/URI/URL/http.pm
@@ -18,7 +18,8 @@ __END__
 
 # Note that the following two methods does not return the old
 # value if they are used to set a new value.
-# The risk of croaking is to high :-)
+# The risk of croaking is to high :-)  We will eventually rely
+# on undefined wantarray (require perl5.004).
 
 # Handle ...?dog+bones type of query
 sub keywords {
@@ -42,14 +43,15 @@ sub query_form {
 	# Try to set query string
 	my @query;
 	my($key,$vals);
+        my $esc = $URI::URL::reserved . $URI::URL::unsafe;
 	while (($key,$vals) = splice(@_, 0, 2)) {
 	    $key = '' unless defined $key;
-	    $key =  URI::Escape::uri_escape($key, $URI::URL::reserved);
+	    $key =  URI::Escape::uri_escape($key, $esc);
 	    $vals = [$vals] unless ref($vals) eq 'ARRAY';
 	    my $val;
 	    for $val (@$vals) {
 		$val = '' unless defined $val;
-		$val = URI::Escape::uri_escape($val, $URI::URL::reserved);
+		$val = URI::Escape::uri_escape($val, $esc);
 		push(@query, "$key=$val");
 	    }
 	}
@@ -59,7 +61,7 @@ sub query_form {
     return undef unless defined $old;
     return () unless length $old;
     Carp::croak("Query is not a form") unless $old =~ /=/;
-    map { URI::Escape::uri_unescape($_) }
+    map { s/\+/ /g; URI::Escape::uri_unescape($_) }
 	 map { split(/=/, $_, 2)} split(/&/, $old);
 }
 

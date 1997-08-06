bce48055bf50f6817f83965ef6c265fd621db750
Gisle Aas <aas@oslonett.no>
1997-08-06 08:20:16 +0000

    We use 5.004 now.

--- lib/Bundle/LWP.pm
+++ lib/Bundle/LWP.pm
@@ -1,6 +1,6 @@
 package Bundle::LWP;
 
-$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.2 $ =~ /(\d+)\.(\d+)/);
 
 1;
 
@@ -17,8 +17,6 @@ Bundle::LWP - A bundle to install all libwww-perl related modules
 =head1 CONTENTS
 
 Net::FTP 2.00      - If you want ftp://-support
-
-IO::Socket 1.13    - Used by HTTP::Daemon
 
 MIME::Base64       - Used in authentication headers
 

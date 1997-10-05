30c3ca10b76a3b30e0747c9a5afbcb54ca6a4eb8
Gisle Aas <aas@oslonett.no>
1997-10-05 12:04:54 +0000

    Don't support ::URL::COMPAT_VER_3 any more.  defined(wantarray) protects path/query

--- lib/URI/URL/_generic.pm
+++ lib/URI/URL/_generic.pm
@@ -245,21 +245,8 @@ sub epath {
 sub path {
     my $self = shift;
     my $old = $self->_elem('path',
-			   map { uri_escape($_,
-					    $URI::URL::reserved_no_slash)
-			       } @_);
-
-    if ($URI::URL::COMPAT_VER_3) {
-	# We used to get rid of the leading "/" in the path
-	if (defined $old) {
-	    $old =~ s|^/||;
-	    Carp::croak("Path components contain '/' (you must call epath)")
-		if $old =~ /%2[fF]/;
-	    return uri_unescape($old);
-	}
-	return undef;
-    }
-
+		      map uri_escape($_, $URI::URL::reserved_no_slash), @_);
+    return unless defined wantarray;
     return '/' if !defined($old) || !length($old);
     Carp::croak("Path components contain '/' (you must call epath)")
 	if $old =~ /%2[fF]/ and !@_;
@@ -295,7 +282,7 @@ sub equery   { shift->_elem('query',   @_); }
 sub query {
     my $self = shift;
     my $old = $self->_elem('query', map { uri_escape($_, $URI::URL::reserved_no_form) } @_);
-    if (defined $old) {
+    if (defined(wantarray) && defined($old)) {
 	if ($old =~ /%(?:26|2[bB]|3[dD])/) {  # contains escaped '=' '&' or '+'
 	    my $mess;
 	    for ($old) {

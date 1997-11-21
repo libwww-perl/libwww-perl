1a3dea7c0b04c451fc893cac93f8de3486da2d76
Gisle Aas <aas@oslonett.no>
1997-11-21 00:29:41 +0000

    str2time was not imported any more.  Could not call head() in array context.

--- lib/LWP/Simple.pm
+++ lib/LWP/Simple.pm
@@ -1,5 +1,5 @@
 #
-# $Id: Simple.pm,v 1.24 1997/10/16 11:13:08 aas Exp $
+# $Id: Simple.pm,v 1.25 1997/11/21 00:29:41 aas Exp $
 
 =head1 NAME
 
@@ -159,7 +159,7 @@ use HTTP::Status;
 push(@EXPORT, @HTTP::Status::EXPORT);
 
 
-$VERSION = sprintf("%d.%02d", q$Revision: 1.24 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.25 $ =~ /(\d+)\.(\d+)/);
 $FULL_LWP++ if grep {$_ eq "http_proxy"} keys %ENV;
 
 
@@ -217,8 +217,8 @@ sub head ($)
 	return $response unless wantarray;
 	return ($response->header('Content-Type'),
 		$response->header('Content-Length'),
-		str2time($response->header('Last-Modified')),
-		str2time($response->header('Expires')),
+		HTTP::Date::str2time($response->header('Last-Modified')),
+		HTTP::Date::str2time($response->header('Expires')),
 		$response->header('Server'),
 	       );
     }

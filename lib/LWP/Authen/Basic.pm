c2c8dc9d1124a66819f5f1ac8e49bb50391725cc
Gisle Aas <aas@oslonett.no>
1997-12-01 15:50:56 +0000

    Reindented.

--- lib/LWP/Authen/Basic.pm
+++ lib/LWP/Authen/Basic.pm
@@ -11,28 +11,27 @@ sub authenticate
 
     my($user, $pass) = $ua->get_basic_credentials($auth_param{realm},
                                                   $request->url, $proxy);
-    if (defined $user and defined $pass) {
-	my $auth_header = $proxy ? "Proxy-Authorization" : "Authorization";
-	my $auth_value = "Basic " . MIME::Base64::encode("$user:$pass", "");
+    return $response unless defined $user and defined $pass;
 
-	# Need to check this isn't a repeated fail!
-	my $r = $response;
-	while ($r) {
-	    my $auth = $r->request->header($auth_header);
-	    if ($auth && $auth eq $auth_value) {
-		# here we know this failed before
-		$response->header("Client-Warning" =>
-				  "Credentials for '$user' failed before");
-		return $response;
-	    }
-	    $r = $r->previous;
+    my $auth_header = $proxy ? "Proxy-Authorization" : "Authorization";
+    my $auth_value = "Basic " . MIME::Base64::encode("$user:$pass", "");
+
+    # Need to check this isn't a repeated fail!
+    my $r = $response;
+    while ($r) {
+	my $auth = $r->request->header($auth_header);
+	if ($auth && $auth eq $auth_value) {
+	    # here we know this failed before
+	    $response->header("Client-Warning" =>
+			      "Credentials for '$user' failed before");
+	    return $response;
 	}
-
-	my $referral = $request->clone;
-	$referral->header($auth_header => $auth_value);
-	
-	return $ua->request($referral, $arg, $size, $response);
+	$r = $r->previous;
     }
+
+    my $referral = $request->clone;
+    $referral->header($auth_header => $auth_value);
+    return $ua->request($referral, $arg, $size, $response);
 }
 
 1;

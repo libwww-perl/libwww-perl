743b296280de7c049d946e0f2d5830235e6fcd26
Gisle Aas <aas@oslonett.no>
1997-12-16 19:47:57 +0000

    New implementation based on Net::NNTP

--- lib/LWP/Protocol/nntp.pm
+++ lib/LWP/Protocol/nntp.pm
@@ -1,5 +1,5 @@
 #
-# $Id: nntp.pm,v 1.5 1996/07/08 13:37:01 aas Exp $
+# $Id: nntp.pm,v 1.6 1997/12/16 19:47:57 aas Exp $
 
 # Implementation of the Network News Transfer Protocol (RFC 977)
 #
@@ -9,18 +9,12 @@ package LWP::Protocol::nntp;
 require LWP::Protocol;
 @ISA = qw(LWP::Protocol);
 
-require LWP::Socket;
 require LWP::Debug;
-
-require HTTP::Request;
 require HTTP::Response;
-use HTTP::Status ();
+require HTTP::Status;
+require Net::NNTP;
 
 use strict;
-use vars qw($NNTP_SERVER $NNTP_PORT);
-
-$NNTP_SERVER = $ENV{NNTP_SERVER} || "news";
-$NNTP_PORT   = 119;
 
 
 sub request
@@ -63,13 +57,17 @@ sub request
     }
 
     # Create a socket and connect to the NNTP server.  We use our own
-    # specialization of the LWP::Socket class.  This new class is defined
-    # below.
-    my $nntp = new LWP::Protocol::nntp::Socket;  # What an ugly name
-    $nntp->connect($NNTP_SERVER, $NNTP_PORT);
+    # specialization of the IO::Socket::INET class.  This new class is
+    # defined below.
+    my $nntp = Net::NNTP->new(undef,
+			      #Port    => 18574,
+			      Timeout => $timeout,
+			      Debug   => 1,
+			     );
+    die "Can't connect to nntp server" unless $nntp;
 
     # Check the initial welcome message from the NNTP server
-    if ($nntp->response($timeout) != 2) {
+    if ($nntp->status != 2) {
 	return HTTP::Response->new(&HTTP::Status::RC_SERVICE_UNAVAILABLE,
 				   $nntp->message);
     }
@@ -83,33 +81,18 @@ sub request
     # this works for our server.
     $mess =~ s/\s+ready\b.*//;
     $mess =~ s/^\S+\s+//;
-    $response->header('Server', $mess);
+    $response->header(Server => $mess);
+
 
     # First we handle posting of articles
     if ($method eq 'POST') {
-	$request->header("Newsgroups", $groupart)
-	    unless $request->header("Newsgroups");
-	if ($nntp->cmd("POST") != 3) {
-	    return HTTP::Response->new(&HTTP::Status::RC_FORBIDDEN,
-				       $nntp->message);
-	}
-	$nntp->write($request->headers_as_string("\015\012") . "\015\012");
-	my $content = $request->content;
-	$content =~ s/^\./../gm;  # must escape "." at the beginning of lies
-	$nntp->write($content);
-	$nntp->write("\015\012.\015\012"); 	# Terminate message
-	if ($nntp->response != 2) {
-	    return HTTP::Response->new(&HTTP::Status::RC_BAD_REQUEST,
-				       $nntp->message);
-	}
-	$response->code(&HTTP::Status::RC_ACCEPTED);
-	$response->message($nntp->message);
-	return $response;
+	return HTTP::Response->new(&HTTP::Status::RC_NOT_IMPLEMENTED,
+				   "POST not implemented yet");
     }
 
     # The method must be "GET" or "HEAD" by now
     if (!$is_art) {
-	if ($nntp->cmd("GROUP $groupart") != 2) {
+	if (!$nntp->group($groupart)) {
 	    return HTTP::Response->new(&HTTP::Status::RC_NOT_FOUND,
 				       $nntp->message);
 	}
@@ -118,29 +101,28 @@ sub request
     }
 
     # Send command to server to retrieve an article (or just the headers)
-    my $cmd = ($method eq 'HEAD' ? 'HEAD' : 'ARTICLE') . " <$groupart>";
-    if ($nntp->cmd($cmd, $timeout) != 2) {
+    my $get = $method eq 'HEAD' ? "head" : "article";
+    my $art = $nntp->$get("<$groupart>");
+    unless ($art) {
 	return HTTP::Response->new(&HTTP::Status::RC_NOT_FOUND,
 				   $nntp->message);
     }
     LWP::Debug::debug($nntp->message);
-
-    # Must read article data until ".".  Here we just "fake" it using the
-    # read_until method.  We should really use the normal read() method
-    # so that we are able to handle real big articles much better.
-    $nntp->read_until('\015?\012\.\015?\012', \$mess, $size, $timeout);
-
-    my($headers, $body) = split(/(?:\015?\012){2}/, $mess, 2);
-
+    
     # Parse headers
     my($key, $val);
-    for (split(/\015?\012/, $headers)) {
-	if (/^(\S+):\s*(.*)/) {
+    while ($_ = shift @$art) {
+	if (/^\s+$/) {
+	    last;
+	} elsif (/^(\S+):\s*(.*)/) {
 	    $response->push_header($key, $val) if $key;
 	    ($key, $val) = ($1, $2);
 	} elsif (/^\s+(.*)/) {
 	    next unless $key;
 	    $val .= $1;
+	} else {
+	    print "XXXX: $_\n";
+	    last;
 	}
     }
     $response->push_header($key, $val) if $key;
@@ -150,58 +132,14 @@ sub request
 	unless $response->header("Content-Type");
 
     # Collect the body
-    if (defined $body) {
-	$body =~ s/\r//g;
-	$body =~ s/^\.\././gm;
-	$response = $self->collect_once($arg, $response, $body);
-    }
+    $response = $self->collect_once($arg, $response, join("", @$art))
+      if @$art;
 
     # Say godbye to the server
-    $nntp->cmd("QUIT");
+    $nntp->quit;
     $nntp = undef;
 
     $response;
 }
-
-# Out special NNTP socket class.  This is just like LWP::Sockets, but
-# implement a few new methods.
-#
-#    $sock->cmd("CMD", $timeout);  # Sends command to server and return the
-#                                  # first digit of the response code
-#
-#    $sock->response($timeout);    # Read response line from server and
-#                                  # return first digit of the response code.
-#
-#    $sock->code                   # Return the full response code (last one)
-#    $sock->message                # Return response message.
-#
-
-package LWP::Protocol::nntp::Socket;
-use vars qw(@ISA);
-@ISA = qw(LWP::Socket);
-
-sub cmd {
-    my($self, $cmd, $timeout) = @_;
-    $self->write("$cmd\015\012", $timeout);
-    $self->response($timeout);
-}
-
-
-sub response {
-    my($self, $timeout) = shift;
-    my $resp;
-    $self->read_until("\015?\012", \$resp, undef, $timeout);
-    $resp =~ s/^(\d{3})\s*//;
-    my $code = $1;
-    $self->{nntp_message} = $resp;
-    $self->{nntp_code} = $code;
-    substr($code, 0, 1);
-}
-
-sub message { shift->{'nntp_message'}; }
-sub code    { shift->{'nntp_code'};    }
-
-
-package LWP::Protocol::nntp;
 
 1;

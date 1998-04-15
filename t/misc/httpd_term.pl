3f8cd272c690f075b195c28ebea331b61e9ea498
Gisle Aas <aas@oslonett.no>
1998-04-15 13:51:10 +0000

    Make it possible to abort the connection as well as the response.

--- t/misc/httpd_term.pl
+++ t/misc/httpd_term.pl
@@ -3,15 +3,18 @@
 use HTTP::Daemon;
 #$HTTP::Daemon::DEBUG++;
 
-my $d = new HTTP::Daemon;
+my $d = HTTP::Daemon->new(Timeout => 60);
 print "Please contact me at: <URL:", $d->url, ">\n";
 
 while (my $c = $d->accept) {
+  CONNECTION:
     while (my $r = $c->get_request) {
 	print $r->as_string;
 	$c->autoflush;
+      RESPONSE:
         while (<STDIN>) {
-	    last if $_ eq ".\n";
+	    last RESPONSE if $_ eq ".\n";
+	    last CONNECTION if $_ eq "..\n";
 	    print $c $_;
 	}
 	print "\nEOF\n";

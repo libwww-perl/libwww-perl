96ce5c6a3c923f708a86db0d1ee36f5578bb4d33
Gisle Aas <aas@oslonett.no>
1998-11-21 23:28:55 +0000

    5.42

--- lib/LWP.pm
+++ lib/LWP.pm
@@ -1,9 +1,9 @@
 #
-# $Id: LWP.pm,v 1.84 1998/11/19 22:04:48 aas Exp $
+# $Id: LWP.pm,v 1.85 1998/11/21 23:28:55 aas Exp $
 
 package LWP;
 
-$VERSION = "5.41";
+$VERSION = "5.42";
 sub Version { $VERSION; }
 
 require 5.004;

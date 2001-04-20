851fda317936b3acf3bf3c8138928e37598a8a55
Gisle Aas <gisle@aas.no>
2001-04-20 15:17:33 +0000

    First revision.

---
+++ lib/LWP/ConnCache.pm
@@ -0,0 +1,138 @@
+package LWP::ConnCache;
+
+# $Id: ConnCache.pm,v 1.1 2001/04/20 15:17:33 gisle Exp $
+
+use strict;
+use vars qw($VERSION $DEBUG);
+
+$VERSION = "0.01";
+
+sub new {
+    my($class, %cnf) = @_;
+    die "NYI" if %cnf;
+    my $self = bless { conns => [] }, $class;
+    $self->capacity(undef, 2);
+    $self;
+}
+
+sub deposit {
+    my($self, $type, $key, $conn) = @_;
+    push(@{$self->{conns}}, [$conn, $type, $key, time]);
+    $self->enforce_limits($type);
+    return;
+}
+
+sub withdraw {
+    my($self, $type, $key) = @_;
+    my $conns = $self->{conns};
+    for my $i (0 .. @$conns - 1) {
+	my $c = $conns->[$i];
+	next unless $c->[1] eq $type && $c->[2] eq $key;
+	splice(@$conns, $i, 1);  # remove it
+	return $c->[0];
+    }
+    return undef;
+}
+
+sub capacity {  # limit
+    my $self = shift;
+    my $type = shift;
+    $type = "*TOTAL*" unless defined $type;
+    my $old = $self->{limit}{$type};
+    if (@_) {
+	$self->{limit}{$type} = shift;
+    }
+    $old;
+}
+
+sub enforce_limits {
+    my($self, $type) = @_;
+    my $conns = $self->{conns};
+
+    my @types = $type ? ($type) : ($self->get_types);
+    for $type (@types) {
+	my $limit = $self->{limit}{$type};
+	next unless defined $limit;
+	for my $i (reverse 0 .. @$conns - 1) {
+	    next unless $conns->[$i][1] eq $type;
+	    if (--$limit < 0) {
+		$self->dropping(splice(@$conns, $i, 1), "$type capacity exceeded");
+	    }
+	}
+    }
+
+    if (defined(my $total = $self->{limit}{'*TOTAL*'})) {
+	while (@$conns > $total) {
+	    $self->dropping(shift(@$conns), "Total capacity exceeded");
+	}
+    }
+}
+
+sub dropping {
+    my($self, $c, $reason) = @_;
+    print "DROPPING @$c [$reason]\n" if $DEBUG;
+}
+
+sub drop {
+    my($self, $checker, $reason) = @_;
+    if (ref($checker) ne "CODE") {
+	# make it so
+	if (!defined $checker) {
+	    $checker = sub { 1 };  # drop all of them
+	}
+	elsif (_looks_like_number($checker)) {
+	    my $age_limit = $checker;
+	    my $time_limit = time - $age_limit;
+	    $reason ||= "older than $age_limit";
+	    $checker = sub { $_[3] < $time_limit };
+	}
+	else {
+	    my $type = $checker;
+	    $reason ||= "drop $type";
+	    $checker = sub { $_[1] eq $type };  # match on type
+	}
+    }
+    $reason ||= "drop";
+
+    local $SIG{__DIE__};  # don't interfere with eval below
+    local $@;
+    my @c;
+    for (@{$self->{conns}}) {
+	my $drop;
+	eval {
+	    if (&$checker(@$_)) {
+		$self->dropping($_, $reason);
+		$drop++;
+	    }
+	};
+	push(@c, $_) unless $drop;
+    }
+    @{$self->{conns}} = @c;
+}
+
+sub prune {
+    my $self = shift;
+    $self->drop(sub { !shift->ping }, "ping");
+}
+
+sub get_types {
+    my $self = shift;
+    my %t;
+    $t{$_->[1]}++ for @{$self->{conns}};
+    return keys %t;
+}
+
+sub get_connections {
+    my($self, $type) = @_;
+    my @c;
+    for (@{$self->{conns}}) {
+	push(@c, $_->[0]) if !$type || ($type && $type eq $_->[1]);
+    }
+    @c;
+}
+
+sub _looks_like_number {
+    $_[0] =~ /^([+-]?)(?=\d|\.\d)\d*(\.\d*)?([Ee]([+-]?\d+))?$/;
+}
+
+1;

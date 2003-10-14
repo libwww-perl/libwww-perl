1aebce35159fb359ecb072556ae9418108b1bba0
Gisle Aas <gisle@aas.no>
2003-10-14 12:12:55 +0000

    Claim copyright extension.

--- lib/LWP.pm
+++ lib/LWP.pm
@@ -1,5 +1,5 @@
 #
-# $Id: LWP.pm,v 1.124 2003/10/13 20:46:30 gisle Exp $
+# $Id: LWP.pm,v 1.125 2003/10/14 12:13:02 gisle Exp $
 
 package LWP;
 
@@ -602,7 +602,7 @@ Koenig, Jared Rhine, and Jack Shirazi.
 
 =head1 COPYRIGHT
 
-  Copyright 1995-2001, Gisle Aas
+  Copyright 1995-2003, Gisle Aas
   Copyright 1995, Martijn Koster
 
 This library is free software; you can redistribute it and/or

32c6946ebe53000a9ba94d257cc64a7d0dcf5698
Gisle Aas <gisle@aas.no>
2004-06-14 12:57:07 +0000

    Return other parameters; $arg, $size, $timeout.

--- lib/LWP/Protocol/loopback.pm
+++ lib/LWP/Protocol/loopback.pm
@@ -13,8 +13,12 @@ sub request {
     my $response = HTTP::Response->new(200, "OK");
     $response->content_type("message/http; msgtype=request");
 
-    $response->push_header("Via", "loopback/1.0 $proxy")
+    $response->header("Via", "loopback/1.0 $proxy")
 	if $proxy;
+
+    $response->header("X-Arg", $arg);
+    $response->header("X-Read-Size", $size);
+    $response->header("X-Timeout", $timeout);
 
     return $self->collect_once($arg, $response, $request->as_string);
 }

724e29d4e8764aee9580398cb52c5ee7e896957f
Gisle Aas <gisle@aas.no>
2009-01-01 07:51:53 +0100

    Bring back the LWP::Debug code [RT#41759]
    Having the code around allow various 3rd party modules (like LWP::UserAgent::Determined)
    to continue to run.

--- lib/LWP/Debug.pm
+++ lib/LWP/Debug.pm
@@ -1,6 +1,67 @@
-package LWP::Debug;
+package LWP::Debug;  # legacy
 
-# legacy stub
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw(level trace debug conns);
+
+use Carp ();
+
+my @levels = qw(trace debug conns);
+%current_level = ();
+
+
+sub import
+{
+    my $pack = shift;
+    my $callpkg = caller(0);
+    my @symbols = ();
+    my @levels = ();
+    for (@_) {
+	if (/^[-+]/) {
+	    push(@levels, $_);
+	}
+	else {
+	    push(@symbols, $_);
+	}
+    }
+    Exporter::export($pack, $callpkg, @symbols);
+    level(@levels);
+}
+
+
+sub level
+{
+    for (@_) {
+	if ($_ eq '+') {              # all on
+	    # switch on all levels
+	    %current_level = map { $_ => 1 } @levels;
+	}
+	elsif ($_ eq '-') {           # all off
+	    %current_level = ();
+	}
+	elsif (/^([-+])(\w+)$/) {
+	    $current_level{$2} = $1 eq '+';
+	}
+	else {
+	    Carp::croak("Illegal level format $_");
+	}
+    }
+}
+
+
+sub trace  { _log(@_) if $current_level{'trace'}; }
+sub debug  { _log(@_) if $current_level{'debug'}; }
+sub conns  { _log(@_) if $current_level{'conns'}; }
+
+
+sub _log
+{
+    my $msg = shift;
+    $msg .= "\n" unless $msg =~ /\n$/;  # ensure trailing "\n"
+
+    my($package,$filename,$line,$sub) = caller(2);
+    print STDERR "$sub: $msg";
+}
 
 1;
 
@@ -13,6 +74,5 @@ LWP::Debug - deprecated
 =head1 DESCRIPTION
 
 LWP::Debug used to provide tracing facilities, but these are not used
-by LWP any more.  Currently it's just an empty do-nothing module so
-that code that still happen to use it continue to run.
-
+by LWP any more.  This code is kept around so that 3rd party code that
+happen to use it continue to run.

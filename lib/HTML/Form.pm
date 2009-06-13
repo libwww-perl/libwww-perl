libwww-perl/libwww-perl

commit e4380a1385f36a139977e61b1a7e0e72f42746c8

Also need to encode strings in the latin1 range
In general we never want the semantics to depend on the presence of the
UTF-8 flag on strings.

lib/HTML/Form.pm
@@ -660,7 +660,7 @@ sub make_request
     my $charset = $self->accept_charset eq "UNKNOWN" ? 'utf-8' : $self->accept_charset;
     if ($Encode_available) {
         foreach my $fi (@form) {
-            $fi = Encode::encode($charset, $fi) if utf8::is_utf8($fi);
+            $fi = Encode::encode($charset, $fi);
         }
     }
 

t/html/form-unicode.t
@@ -21,6 +21,7 @@ $SIG{__WARN__} = sub { push(@warn, $_[0]) };
 my $f = HTML::Form->parse(<<'EOT', "http://localhost/");
 <form action="abc" name="foo">
 <input name="name">
+<input name="latin">
 </form>
 <form></form>
 EOT
@@ -28,28 +29,29 @@ EOT
 ok($f->value("name"), "");
 ok($f->accept_charset, "UNKNOWN");
 my $req = $f->click;
-ok($req->uri, "http://localhost/abc?name=");
+ok($req->uri, "http://localhost/abc?name=&latin=");
 
 $f->value(name => "\x{0424}");  # capital cyrillic ef
+$f->value(latin => "\xE5");     # aring
 $req = $f->click;
 ok($req->method, "GET");
-ok($req->uri, "http://localhost/abc?name=%D0%A4");
+ok($req->uri, "http://localhost/abc?name=%D0%A4&latin=%C3%A5");
 
 $f->method('POST');
 $f->enctype('multipart/form-data');
 
 $req = $f->click;
 ok($req->uri, "http://localhost/abc");
-ok($req->content, "--xYzZY\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\n\xD0\xA4\r\n--xYzZY--\r\n");
+ok($req->content, "--xYzZY\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\n\xD0\xA4\r\n--xYzZY\r\nContent-Disposition: form-data; name=\"latin\"\r\n\r\n\xC3\xA5\r\n--xYzZY--\r\n");
 
 $f->accept_charset('koi8-r');
 $req = $f->click;
 ok($req->uri, "http://localhost/abc");
-ok($req->content, "--xYzZY\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\n\xE6\r\n--xYzZY--\r\n");
+ok($req->content, "--xYzZY\r\nContent-Disposition: form-data; name=\"name\"\r\n\r\n\xE6\r\n--xYzZY\r\nContent-Disposition: form-data; name=\"latin\"\r\n\r\n?\r\n--xYzZY--\r\n");
 
 $f->method('GET');
 $req = $f->click;
-ok($req->uri, "http://localhost/abc?name=%E6");
+ok($req->uri, "http://localhost/abc?name=%E6&latin=%3F");
 
 $f = HTML::Form->parse(<<'EOT', "http://localhost/");
 <form action="abc" name="foo" accept-charset="koi8-r">

commit 7f01b76b01f6ec102bfe863064e5bc1150bed804

Files are passed as an array and we must not stringify it.
Fixes breakage introduced by change e4380a1385f36a139977e61b1a7e0e72f42746c8

lib/HTML/Form.pm
@@ -664,7 +664,7 @@ sub make_request
     my $charset = $self->accept_charset eq "UNKNOWN" ? 'utf-8' : $self->accept_charset;
     if ($Encode_available) {
         foreach my $fi (@form) {
-            $fi = Encode::encode($charset, $fi);
+            $fi = Encode::encode($charset, $fi) unless ref($fi);
         }
     }
 
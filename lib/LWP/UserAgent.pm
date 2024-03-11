97d28846cfe0806c0b8dbc1c203fb7102f406009
Olaf Alders <olaf@wundersolutions.com>
2024-03-10 20:51:27 -0400

    Add more links to Pod

--- lib/LWP/UserAgent.pm
+++ lib/LWP/UserAgent.pm
@@ -1623,11 +1623,11 @@ This option is initialized from the C<PERL_LWP_SSL_VERIFY_HOSTNAME> environment
 variable.  If this environment variable isn't set; then C<verify_hostname>
 defaults to 1.
 
-Please note that that recently the overall effect of this option with regards to 
-SSL handling has changed. As of version 6.11 of LWP::Protocol::https, which is an 
+Please note that that recently the overall effect of this option with regards to
+SSL handling has changed. As of version 6.11 of L<LWP::Protocol::https>, which is an
 external module, SSL certificate verification was harmonized to behave in sync with
-IO::Socket::SSL. With this change, setting this option no longer disables all SSL
-certificate verification, only the hostname checks. To disable all verification
+L<IO::Socket::SSL>. With this change, setting this option no longer disables all SSL
+certificate verification, only the hostname checks. To disable all verification,
 use the C<SSL_verify_mode> option in the C<ssl_opts> attribute. For example:
 C<$ua->ssl_opts(SSL_verify_mode => IO::Socket::SSL::SSL_VERIFY_NONE);>
 
